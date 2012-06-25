7e9d61ad3f53200f0a8cb179d3be1c59810c5b1e
Himeshi De Silva <himeshionfb@gmail.com>
2012-06-25 10:11:03 +0530

    Initializing of all applications

--- global/apploader.js
+++ global/apploader.js
@@ -1,4 +1,15 @@
 define([ './global/applications/ApplicationA/application',
 		'./global/applications/ApplicationB/application' ], function(
 		ApplicationA, ApplicationB) {
+		
+	return funtion(){
+		this.init() = function () {
+			
+			var apps=new Array(new ApplicationA();, new ApplicationB();); // Array containing all the applications
+			
+			for(var count=0;count<apps.length;count++){
+				apps[count].init(); //initializes all the applications
+			}
+		};
+	};
 });
