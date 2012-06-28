dbe97c6f0d2995b5735f86558ee3eec4e5c02a0f
slayerjay <99xcjwe@99XCW7JWE.99x.local>
2012-06-28 09:11:05 +0530

    Restructured the folder structure and test loading for better modularity.

--- tests/tests.js
+++ tests/tests.js
@@ -1,35 +1,9 @@
 require.config({
-	baseUrl : "../src/",	//we will set the application's src folder as the base url
+	baseUrl : "../src/", //we will set the application's src folder as the base url
 });
 
-require([ './apploader' 	//load the modules that will be used in the tests
+require([ '../tests/testmodules/appLoaderTests', 	//A list of all the unit test module paths (relative to the base URL)
+          '../tests/testmodules/sampleTests'
           ], function() {
-	$(function() {			//wait till the DOM loads, (a workaround to wait till testr.js loads)
-		
-		/*
-		 * Start Unit tests
-		 */
-
-		module("AppLoader Tests");
-		test("Application A init", function() {
-			
-			var fakeInit = sinon.spy();	//spy method
-			var fakeApp = function() {
-				this.init = fakeInit;
-			};
-
-			var AppLoader = testr('apploader', {
-				'./apps/ApplicationA/application' : fakeApp
-			});
-
-			var appLoader = new AppLoader();
-			appLoader.init();
-			ok(fakeInit.called, "The init method in ApplicationA is called");
-		});
-		
-
-	});
 
 });
-
-
