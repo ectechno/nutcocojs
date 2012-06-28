60b8c9b7d79e7f50b4836dd49b528cf7f94d39ce
Himeshi De Silva <himeshionfb@gmail.com>
2012-06-28 10:15:26 +0530

    Registering the paths of localrouters in order to application level routing

--- src/apps/ApplicationA/router.js
+++ src/apps/ApplicationA/router.js
@@ -1,19 +1,21 @@
 //Array containing base paths
-var APP_PATHS = [ 'appAPath1/', 'appAPath2/' ];
+var APP_PATHS_A = [ 'appAPath1/', 'appAPath2/' ];
 
 // Setting up crossroads and initiate listeners
 define(function() {
 	return function() {
 		this.start = function() {
-			var localRouter = crossroads.create(); // Define new router for local routing
-			//alert('Starting local router A');								
+			var localRouter = crossroads.create(); // Define new router for
+													// local routing
+
 			// Initiating listeners for each path
-			for ( var tempPath in APP_PATHS) {
-				//alert(APP_PATHS[tempPath] + '{id}');		
-				localRouter.addRoute(APP_PATHS[tempPath] + '{id}',
-						function(id) {
-							console.log(id);
-						});
+			for ( var tempPath in APP_PATHS_A) {
+
+				// console.log(APP_PATHS_A[tempPath]);
+				localRouter.addRoute(APP_PATHS_A[tempPath] + '{id}', function(
+						id) {
+					console.log("appA" + id);
+				});
 			}
 			return localRouter;
 		};
--- src/apps/ApplicationB/router.js
+++ src/apps/ApplicationB/router.js
@@ -1,18 +1,20 @@
 //Array containing base paths
-var APP_PATHS = [ 'appBPath1/', 'appBPath2/' ];
+var APP_PATHS_B = [ 'appBPath1/', 'appBPath2/' ];
 
 // Setting up crossroads and initiate listeners
 define(function() {
 	return function() {
 		this.start = function() {
-			var localRouter = crossroads.create(); // Define new router for local routing
-			//alert('Starting local router B');								
+
+			var localRouter = crossroads.create(); // Define new router for
+													// local routing
+
 			// Initiating listeners for each path
-			for ( var tempPath in APP_PATHS) {
-				localRouter.addRoute(APP_PATHS[tempPath] + '{id}',
-						function(id) {
-							console.log(id);
-						});
+			for ( var tempPath in APP_PATHS_B) {
+				localRouter.addRoute(APP_PATHS_B[tempPath] + '{id}', function(
+						id) {
+					console.log("appB" + id);
+				});
 			}
 			return localRouter;
 		};
