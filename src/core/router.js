0a160ae0d271916e2d52f4b7dbde15a1882c1238
hasith <hasithy@99x.lk>
2012-07-02 15:59:11 +0530

    simplified router class removing inheritance friendlyness

--- src/core/router.js
+++ src/core/router.js
@@ -1,35 +1,33 @@
 ﻿define(['_util'], function(Util) {
 	
-	var router = function(parent) {
-		this.router = crossroads.create();
-		this.parent = parent;
+	return function(parent) {
+		var _router = crossroads.create();
+		var _parent = parent;
 		
-		this.getNativeRouter = function() {return this.router;};
-		this.initialize.apply(this, arguments);
-	};
+		return{
+			addRoute : function(pattern, handler) {
+				return _router.addRoute(pattern, handler);
+			},
+			
+			init : function() {
+				
+				if (_parent) {
+					crParentRouter = _parent.getNativeRouter();
+					crParentRouter.routed.add(_router.parse, _router);
+					crParentRouter.bypassed.add(_router.parse, _router);
+				} else if(!hasher.isActive()) {
+					function parseHash(newHash, oldHash) {
+						_router.parse(newHash);
+					}
+					hasher.initialized.add(parseHash);// parse initial hash
+					hasher.changed.add(parseHash);// parse hash changes
+					hasher.init(); // start listening for history change
 
-	router.extend = Util.Extend;
-	router.prototype.initialize = function(){};
-	router.prototype.addRoute = function(pattern, handler) {
-		return this.router.addRoute(pattern, handler);
-	};
-	router.prototype.init = function() {
-		
-		if (this.parent) {
-			crParentRouter = this.parent.getNativeRouter();
-			crParentRouter.routed.add(this.router.parse, this.router);
-			crParentRouter.bypassed.add(this.router.parse, this.router);
-		} else if(!hasher.isActive()) {
-			var self = this;
-			function parseHash(newHash, oldHash) {
-				self.router.parse(newHash);
-			}
-			hasher.initialized.add(parseHash);// parse initial hash
-			hasher.changed.add(parseHash);// parse hash changes
-			hasher.init(); // start listening for history change
-
+				};
+			},
+			
+			getNativeRouter : function() {return _router;}
 		};
+		
 	};
-	
-	return router;
 });
