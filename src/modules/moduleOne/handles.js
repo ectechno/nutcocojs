78ee35c3552f0758314f8f6a4c4f81fbeb046470
hasith <hasithy@99x.lk>
2012-07-09 17:14:17 +0530

    corrected some caps issue in paths

--- src/modules/moduleOne/handles.js
+++ src/modules/moduleOne/handles.js
@@ -7,8 +7,8 @@
 define(function(require) {
 
 	// lets get all the panel handler classes in to variables
-	var ListViewPanelHandler = require('./listViewPanel/handler');
-	var DetailViewPanelHandler = require('./detailViewPanel/handler');
+	var ListViewPanelHandler = require('./listviewPanel/handler');
+	var DetailViewPanelHandler = require('./detailviewPanel/handler');
 	
 	// now return a structure containing the route path and handler instance
 	return {
