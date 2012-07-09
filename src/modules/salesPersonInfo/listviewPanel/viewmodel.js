75906426c364a80df1f05e0326ed18b2e4fb1fc3
janith <slayerjay@gmail.com>
2012-07-09 16:52:42 +0530

    Added the listview.

--- src/modules/salesPersonInfo/listviewPanel/viewmodel.js
+++ src/modules/salesPersonInfo/listviewPanel/viewmodel.js
@@ -1,8 +1,25 @@
 define([], function() {
 
-	return function(id) {
-		//TODO
+	return function() {
+		var self = this;
+		
+		this.init = function(){
+			//TODO: need to get the url from the settings object
+			$.getJSON("./dummyServer/salesPersonInfo.json", function (result) {
+				self.salesPersons(result);
+            });
+		}	
+			
+		
+		
+		this.salesPersons = ko.observableArray();
+		this.personClicked = function(person){
+			//TODO: need to user router.setRoute() method instead
+			hasher.setHash("salespersons/"+person.id);
+		}
 		this.moduleEnabled = ko.observable(true);
-		console.log("Sales Persons list");
+		
+		
+		this.init();	//call the initialization method.. is this the best way?
 	};
 });
