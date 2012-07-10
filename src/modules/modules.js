fb318d4672208510512667534954616d86faa56d
hasith <hasithy@99x.lk>
2012-07-10 16:23:54 +0530

    changed the settings implementation. also changed the loader functionality so that modules are named.

--- src/modules/modules.js
+++ src/modules/modules.js
@@ -1,6 +1,9 @@
 define(function(require) {
 
 	// create an array containing all modules classes that needs to be initiated
-	return [ require('./moduleOne/module'), require('./moduleTwo/module') ];
+	return {
+		moduleOne : require('./moduleOne/module'),
+		moduleTwo : require('./moduleTwo/module')
+	};
 
 });
--- tests/modules/settingsTests.js
+++ tests/modules/settingsTests.js
@@ -1,16 +1,49 @@
 require([ './settings', '_core' ], function(Settings) {
 
+	var Core = null;
 	module("Settings Tests",{
 		setup : function(){
-			var Core = testr('_core');
-			this.context = new Core.Context();
-			this.context.setSettings(Settings);
+			Core = testr('_core');
 		}
 	});
 	
-	test('Test settings', function(){
-		equal(this.context.getSettings(), Settings, 'global settings set');
+
+	
+	test('basic settings usage', function(){
+		var settings = new Core.Settings();
+		settings.load({
+			key1 : "value1",
+			key2 : {key2_1 : "value2.1",},
+		});
 		
+		equal(settings.items()['key1'], "value1");
+		equal(settings.items().key1, "value1");
+		
+		equal(settings.items().key2.key2_1, "value2.1");
+		equal(settings.items()['key2']['key2_1'], "value2.1");
 	});
+	
+	
+	test('chained settings usage', function(){
+		var baseSettings = new Core.Settings();
+		baseSettings.load({
+			key1 : "value1",
+			key2 : {key2_1 : "value2.1",},
+		});
+		
+		var settings = new Core.Settings(baseSettings);
+		settings.load({
+			key1 : "valueOverwriten",
+			key3 : "value3",
+		});
+		
+		equal(settings.items()['key1'], "valueOverwriten");
+		equal(settings.items().key2.key2_1, "value2.1");
+		equal(settings.items().key3, "value3");
+		
+		settings.chainSettings(false);
+		equal(settings.items()['key2'], undefined);
+	});
+	
 });
 
