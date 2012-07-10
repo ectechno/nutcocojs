4ff1501370fbbbaa4f4ea90aa2469615da8fb675
janith <slayerjay@gmail.com>
2012-07-10 15:30:34 +0530

    Modified the salesinfo module files to have a shared view model

---
+++ src/modules/salesInfo/compositePanel/handler.js
@@ -0,0 +1,27 @@
+define([ '_core', './viewmodel', 'text!./view.html', 'text!./chartViewPanel/view.html','text!./treeViewPanel/view.html'],function( Core, ViewModel, parentViewText, chartViewText, treeViewText) {
+	return function(moduleContext) {
+
+		var panel = null;
+		return {
+			
+			activate: function(vals) {
+				var vm = new ViewModel(vals.id);
+				
+				//A workaround to get the shared viewmodel working... Not sure if this is the best way
+				var parent = $(parentViewText);
+				parent.find("#panel1").append(treeViewText);
+				parent.find("#panel2").append(chartViewText);
+				
+				panel = new Core.Panel(vm, parent.html());
+				
+			},
+			
+			deactivate: function() {
+				if (panel) {
+					panel.dispose();
+				}
+			}
+		};
+		
+	};
+});
