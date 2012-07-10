e57ef3a067c125d626af37bf3674b8f76aa13dd0
Asanka <asankan@99x.lk>
2012-07-10 11:43:14 +0530

    Resolved Conflict

--- src/modules/modules.js
+++ src/modules/modules.js
@@ -2,10 +2,8 @@ define(function(require) {
 
 	var moduleOne = require('./moduleOne/module');
 	var moduleTwo = require('./moduleTwo/module');
-	var salesPersonInfo = require('./salesPersonInfo/module');
-	var salesInfo = require('./salesInfo/module');
 
 	// create an array containing all modules for easy handling
-	return [ new moduleOne(), new moduleTwo(), new salesPersonInfo(), new salesInfo() ];
+	return [ new moduleOne(), new moduleTwo() ];
 
 });
