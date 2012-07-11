76c6fa9de3752b49b6092d2440f0b4dfd257dcac
Asanka <asankanissanka@gmail.com>
2012-07-11 16:56:28 +0530

    View table for the selected year

--- src/modules/salesInfo/compositePanel/viewmodel.js
+++ src/modules/salesInfo/compositePanel/viewmodel.js
@@ -1,46 +1,3 @@
-<<<<<<< HEAD
-define([], function() {
-
-	return function(moduleContext) {
-		var self = this;
-		this.year=ko.observable();
-		this.deptName=ko.observable();
-		this.month=ko.observable("Test");
-		this.sales=ko.observable("TestSales");
-		
-		this.init = function(){
-			$.getJSON(moduleContext.getSettings().items().urls.departments, function (result) {
-				self.salesInfo(result);
-            });
-		}	
-		
-		this.salesInfo=ko.observableArray();
-		this.moduleEnabled = ko.observable(true);
-		this.init();
-		self.drawTree = function(){
-			//alert('drawing tree');
-			$("#treeView").jstree({
-	            "themes": {
-	                "theme": "apple"
-	            },
-	            "plugins": ["themes", "html_data"]
-	        });
-		}
-		
-		this.yearClicked= function(years,department,data){
-			console.log(years + ' ' + department+' '+ data.values);
-			self.setTableValues(years,department,data);
-		}
-		
-		//Function to set the table values
-		this.setTableValues=function(year,department,data){
-			self.year(year);
-			self.deptName(department);
-		}
-		
-	};
-});
-=======
 define([], function() {
 
 	return function(moduleContext) {
@@ -54,6 +11,7 @@ define([], function() {
 		this.selectedYear = ko.observable();
 		this.selectedDept = ko.observable();
 		this.selectedData = ko.observableArray();
+		
 		this.totalSales = ko.computed(function(){
 			return _.reduce(self.selectedData().values, function(memo, item){ return memo + item.sales; }, 0);
 		});
@@ -61,6 +19,7 @@ define([], function() {
 		this.salesInfo=ko.observableArray();
 		this.moduleEnabled = ko.observable(true);
 		this.init();
+		
 		self.drawTree = function(){
 			//alert('drawing tree');
 			$("#treeView").jstree({
@@ -72,9 +31,9 @@ define([], function() {
 		}
 		
 		this.yearClicked= function(years,department,data){
-			this.selectedYear(years);
-			this.selectedDept(department);
-			this.selectedData(data);
+			self.selectedYear(years);
+			self.selectedDept(department);
+			self.selectedData(data.values);
 			console.log(data.values);
 
 		}
@@ -82,4 +41,4 @@ define([], function() {
 		
 	};
 });
->>>>>>> origin/initdeploy
+
