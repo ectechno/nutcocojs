b1a66a4ee83eccce29f631e01085143b0a7e7147
Asanka <asankanissanka@gmail.com>
2012-07-11 09:15:12 +0530

    Modified shared viewModel implementation

--- src/modules/salesInfo/compositePanel/handler.js
+++ src/modules/salesInfo/compositePanel/handler.js
@@ -8,11 +8,17 @@ define([ '_core', './viewmodel', 'text!./view.html', 'text!./chartViewPanel/view
 				var vm = new ViewModel(vals.id);
 				
 				//A workaround to get the shared viewmodel working... Not sure if this is the best way
-				var parent = $(parentViewText);
-				parent.find("#panel1").append(treeViewText);
-				parent.find("#panel2").append(chartViewText);
+				//var parent = $(parentViewText);
+				//parent.find("#panel1").append(treeViewText);
+				//parent.find("#panel2").append(chartViewText);
 				
-				panel = new Core.Panel(vm, parent.html());
+				//panel = new Core.Panel(vm, parent.html());
+				panel = new Core.Panel(vm, parentViewText);
+				treeViewPanel = new Core.Panel(vm, treeViewText);
+				chartViewPanel = new Core.Panel(vm, chartViewText);
+				
+				$("#panel").append($('#'+treeViewPanel.getElementId()));
+				$("#panel").append($('#'+chartViewPanel.getElementId()));
 				
 			},
 			
