505f1080b219d8aad2e22880381be04355fa036d
Asanka <asankanissanka@gmail.com>
2012-07-11 08:40:26 +0530

    Fixed an error with a path

--- src/modules/moduleOne/handles.js
+++ src/modules/moduleOne/handles.js
@@ -9,6 +9,6 @@ define(function(require) {
 	// now return a structure containing the route path and handler classes
 	return {
 		'listview/{id}' : require('./listviewPanel/handler'),
-		'detailview/{id}' : require('./detailviewPanel/handler')
+		'detailview/{id}' : require('./detailViewPanel/handler')
 	};
 });
