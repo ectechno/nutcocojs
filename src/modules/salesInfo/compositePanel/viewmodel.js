a17e82bdf581e8f4c0f4b72102b34e86cb14e586
Asanka <asankanissanka@gmail.com>
2012-07-13 17:38:13 +0530

    Removed routing from salesinfo module

--- src/modules/salesInfo/compositePanel/viewmodel.js
+++ src/modules/salesInfo/compositePanel/viewmodel.js
@@ -83,10 +83,6 @@ define([], function() {
 		}
 
 		this.yearClicked = function(years, department, data) {
-			// Changing url
-			moduleContext.getController().goTo(
-					"salesinfo/" + department + "/" + years);
-
 			self.tableEnabled(true);
 			self.selectedYear(years);
 			self.selectedDept(department);
