f5162bcf93e1e9423972f0c48546bb5df030909d
Himeshi <himeshionfb@gmail.com>
2012-08-02 17:46:04 +0530

    Update optimizer/Boilerplate_optimizer.bat

--- optimizer/Boilerplate_optimizer.bat
+++ optimizer/Boilerplate_optimizer.bat
@@ -1,3 +1,2 @@
-cd path\to\optimizer
-java -classpath path/to/optimizer/js.jar;path/to/optimizer/compiler.jar org.mozilla.javascript.tools.shell.Main r.js main.js
-node r.js -o path/to/optimizer/boilerplatejs.build.js
+cd path/to/optimizer
+java -classpath path/to/optimizer/js.jar;path/to/optimizer/compiler.jar org.mozilla.javascript.tools.shell.Main r.js -o path/to/optimizer/buildconfig.js
